ac29020367a1e7b33ee2abf889a7c8ab05c6a41b
Hezron-tech <kiprophez@gmail.com>
2022-03-12 00:06:41 +0300

    adding links url

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-
-
 def create_app(app):
 
 
@@ -13,6 +11,23 @@ def create_app(app):
 
 
     db.init_app(app)
+
+    #registering bluebrint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
+
+
+
 
     
 
