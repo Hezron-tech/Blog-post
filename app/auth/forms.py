a095339c53deba4d3c758edf658eab56abd91659
Hezron-tech <kiprophez@gmail.com>
2022-03-12 00:50:30 +0300

    creating registration class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,3 +10,13 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators =[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    username = StringField('Enter your username',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(),
+    EqualTo('password2',message = 'Passwords must match')])
+    password2 = PasswordField('Confirm Passwords',validators = [DataRequired()])
+    submit = SubmitField('Sign Up')    
