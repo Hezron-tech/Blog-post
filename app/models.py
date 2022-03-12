b3e04f31302a95e352c1a1b76d7654a31e7b2221
Hezron-tech <kiprophez@gmail.com>
2022-03-12 21:43:56 +0300

    updating model class--creating subscriber button

--- app/models.py
+++ app/models.py
@@ -1,74 +1,129 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from sqlalchemy.sql import func
-from .import login_manager
-
+from datetime import datetime
+from . import login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))  
-        
-class Quotes:
-    '''
-    Movie class to define Movie Objects
-    '''
-    def __init__(self,author,id,quote,permalink):
-        self.author = author
-        self.id = id
-        self.quote = quote
-        self.permalink = permalink
+    return User.query.get(int(user_id))
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    username = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    posts = db.relationship('Post', backref='user', passive_deletes=True)
-    comments = db.relationship('Comment', backref='user', passive_deletes=True)
-    likes = db.relationship('Like', backref='user', passive_deletes=True)
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    blog = db.relationship("Blog", backref="user", lazy="dynamic")
+    comment = db.relationship("Comment", backref="user", lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash= generate_password_hash(password)
 
 
-class Post(db.Model):
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Blog(db.Model):
+    """
+    List of blogs in each category 
+    """
+    __tablename__ = 'blogs'
+
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey(
-        'user.id', ondelete="CASCADE"), nullable=False)
-    comments = db.relationship('Comment', backref='post', passive_deletes=True)
-    likes = db.relationship('Like', backref='post', passive_deletes=True) 
+    blog_title = db.Column(db.String)
+    blog_content = db.Column(db.String)
+    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment = db.relationship('Comment', backref='blog', lazy="dynamic")
 
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
 
+    def delete_blog(self):
+        db.session.delete(self)
+        db.session.commit()
+        
+    @classmethod
+    def get_blogs(cls, id):
+        blogs = Blog.query.filter_by(user_id = id).order_by(Blog.posted_at.desc()).all()
+        return blogs
 
+    @classmethod
+    def getBlogId(cls, id):
+        blog = Blog.query.filter_by(id=id).first()
+        return blog
+
+    @classmethod
+    def get_all_blogs(cls):
+        return Blog.query.order_by(Blog.posted_at).all()
 
 class Comment(db.Model):
+    """
+    User comment model for each blog 
+    """
+    __tablename__ = 'comments'
+
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey(
-        'user.id', ondelete="CASCADE"), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey(
-        'post.id', ondelete="CASCADE"), nullable=False)  
+    comment = db.Column(db.String(255))
+    comment_date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
 
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
 
-class Like(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey(
-        'user.id', ondelete="CASCADE"), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey(
-        'post.id', ondelete="CASCADE"), nullable=False)             
+    def delete_comment(self):
+        db.session.remove(self)
+        db.session.commit()
 
+    @classmethod
+    def getCommentId(cls, id):
+        comment = Comment.query.filter_by(id=id).first()
+        return comment
 
+    @classmethod
+    def get_comments(self, id):
+        comment = Comment.query.order_by(
+            Comment.comment_date.desc()).filter_by(blog_id=id).all()
+        return comment
 
+class Quote:
+    """
+    Class for creating our random quotes.
+    """
+    def __init__(self, author, quote):
+        self.author = author
+        self.quote = quote
 
+class Subscriber(db.Model):
+    '''
+    model class for subscribers
+    '''
+    __tablename__='subscribers'
 
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
 
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
 
+    def getAllMails(cls):
+        return Subscriber.query.order_by(Subscriber.id).all()
 
-
-    
-
-    
+    def __repr__(self):
+        return f'Subscriber {self.email}'
