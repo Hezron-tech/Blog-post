c7e798227fbd3dabb368ccee1764c4bd2a949b9c
Hezron-tech <kiprophez@gmail.com>
2022-03-12 12:58:11 +0300

    adding requests

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 
 class Config:
 
-    POST_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/quotes.json'
-
-    POPULAR_QUOTE_API_URL='http://quotes.stormconsultancy.co.uk/popular.json'
-    SINGLE_QUOTE_API_URL='http://quotes.stormconsultancy.co.uk/quotes/1.json'
+    
     RANDOM_QUOTE_API_URL='http://quotes.stormconsultancy.co.uk/random.json'
 
    
