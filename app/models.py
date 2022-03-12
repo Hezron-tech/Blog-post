8468e89105b16e93c33b97f75e015f6fd54dd1a9
Hezron-tech <kiprophez@gmail.com>
2022-03-12 13:47:53 +0300

    adding post class

--- app/models.py
+++ app/models.py
@@ -16,6 +16,16 @@ class User(db.Model, UserMixin):
     profile_pic_path = db.Column(db.String())
 
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)    
+
+
 
 
 
